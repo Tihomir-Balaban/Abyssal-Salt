249dcfe57e6c2767e32270d2e16c044f39ab676a
Tihomir-Balaban <161501037+Tihomir-Balaban@users.noreply.github.com>
2026-02-11 17:02:34 +0100

    Add Basket and BasketItems entities with migration
    Introduced new Basket and BasketItems tables along with their relationships. Registered BasketRepository in the dependency injection container to handle basket-related operations.

--- AbySalto.Mid.Infrastructure/DependencyInjection.cs
+++ AbySalto.Mid.Infrastructure/DependencyInjection.cs
@@ -21,6 +21,8 @@ public static class DependencyInjection
             options.UseSqlServer(connectionString));
 
         services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IBasketRepository, BasketRepository>();
+
         return services;
     }
 
